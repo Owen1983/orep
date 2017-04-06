e4d35aeb3d06c023fe5a8e471049485ffe17794c
Owen1983 <oahirst@hotmail.co.uk>
2017-04-06 15:53:28 +0100

    Add files via upload

---
+++ functionInfunction.scala
@@ -0,0 +1,44 @@
+
+/* def function (F:(Int,Int) =>Int)={
+
+	println(F(6,2))
+
+}
+
+
+function((a:Int, b:Int)=> a+b)
+function((a:Int, b:Int)=> a*b)
+function((a:Int, b:Int)=> a/b)
+function((a:Int, b:Int)=> 	if(a>5){
+								a+b*a
+							}else{
+								a*a+b
+							
+							}
+						) 
+						
+def function2(f:(String,String)=>String)={
+
+	println(f("hello", "World"))
+
+}
+
+function2((A:String,B:String)=> A+B)
+function2((A:String,B:String)=> B+A)
+function2((A:String,B:String)=> A+A)
+function2((A:String,B:String)=> B+B)
+ */
+
+def function3(F:(Int,Int=>Int):(Int,Int)=> String ={
+
+
+(a:Int,b:Int)=>"The Result is : " +(a+B)
+}
+
+def funcfunc3():(Int,Int)=> String{
+
+(a:Int,B:Int)=>"The Result is : " +(a+B)
+}
+)
+var F=funcfunc3()
+println(F(7,5))
